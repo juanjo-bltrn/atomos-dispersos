84463e8b4ae92b4133ff33c8e51b1092c7b78666
Juanjo H. Beltrán <115104045+juanjo-bltrn@users.noreply.github.com>
2025-12-02 18:09:01 -0600

    Final version

--- scripts/wallpapers/gradient.js
+++
@@ -1,6 +0,0 @@
-export const gradient = (wallpaperconfig) => { 
-    const wallpaper = document.createElement("div");
-    const colors = wallpaperconfig.colors.join(", ")
-    wallpaper.style = `background: linear-gradient(${wallpaperconfig.rotation}deg, ${colors})`
-    return wallpaper;
-}
